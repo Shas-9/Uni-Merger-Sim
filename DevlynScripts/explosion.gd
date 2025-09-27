646ed54b88aea8aa320ae7a4ba2d52a40e2116c4
DevlynHall <delvyn.hal@gmail.com>
2025-09-27 14:47:12 +0930

    Squashed commit of the following:
    commit c002cd9a53c163e896478147e8df5dfcc655835f
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 14:29:08 2025 +0930
    
        Revert "Merge branch 'player/sharzil' into DevlynsBranch"
    
        This reverts commit abe308bb1304214b37a3737c4aabf6c8f9db2a44, reversing
        changes made to c5a3330b6d6ab13d379a7cb63bbe2cfda4cadd2f.
    
    commit abe308bb1304214b37a3737c4aabf6c8f9db2a44
    Merge: c5a3330 7991c3b
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 14:25:31 2025 +0930
    
        Merge branch 'player/sharzil' into DevlynsBranch
    
    commit c5a3330b6d6ab13d379a7cb63bbe2cfda4cadd2f
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 14:22:30 2025 +0930
    
        merge complete
    
    commit 160ea799a510e8ec3bb99a95c2b8ed7c57fc14d5
    Merge: 6534cff e50b171
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 14:11:30 2025 +0930
    
        Merge branch 'player/sharzil' into DevlynsBranch
    
    commit 6534cff778928d398d132cf63b260f0750af50a1
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 14:00:30 2025 +0930
    
        removed useless gif
    
    commit 126bef7c27813f56d05b55329da8df9e43f26ba9
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 13:43:04 2025 +0930
    
        put everytjhing into folders
    
         into my own folders so it dosnt get messed up on merge
    
    commit 8715f5cd13c9c092de95a835990dbb8b5578dfee
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Sat Sep 27 13:11:03 2025 +0930
    
        finished boss
    
    commit cea700b7b2245a78f35275149e40ca767bddbd26
    Author: DevlynHall <delvyn.hal@gmail.com>
    Date:   Fri Sep 26 18:31:36 2025 +0930
    
        boss initial commit
    
        i forgot to commit for a while
        - boss attack 1
        - player movement by mouse
        - bullets set up
        - 1 bullet type made

---
+++ DevlynScripts/explosion.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var life_time = 2
+func _process(delta: float) -> void:
+	life_time -= delta
+	if self.scale <= Vector2(.5,.5):
+		self.scale += Vector2(0.01, 0.01)
+	else:
+		self.scale -= Vector2(0.01, 0.01)
+	rotate(.01)
+	if ($RayCast2D.is_colliding()):
+		print("hit")
+		var hud = get_tree().get_first_node_in_group("hud")
+		if hud:
+			hud.add_hit()
+	if life_time <= 0:
+		self.scale -= Vector2(0.01, 0.01)
+		self.scale -= Vector2(0.01, 0.01)
+		self.scale -= Vector2(0.01, 0.01)
+		if self.scale <= Vector2(0,0):
+			queue_free()
+		
+	var boss = get_tree().get_first_node_in_group("boss")
+	if !boss:
+		queue_free()
--- scripts/area_2d.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-
-func _mouse_enter() -> void:
-	get_parent().mouseEnter()
