e50b1715a6bb8dfa77f58507cb465fb33d233842
Sharzil <132335949+Shas-9@users.noreply.github.com>
2025-09-27 13:12:29 +0930

    feat: projectile

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -13,4 +13,11 @@ func _process(delta):
 	position += direction * speed * delta
 	$AnimatedSprite2D.play()
 	$AnimatedSprite2D.rotate(10)
+	if not get_viewport().get_visible_rect().has_point(global_position):
+		queue_free()
+		
+	var boss = get_tree().get_first_node_in_group("boss")
+	if boss:
+		if ($RayCast2D.is_colliding() ):
+			boss.take_damage()
 	
