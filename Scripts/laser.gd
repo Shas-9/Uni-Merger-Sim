8715f5cd13c9c092de95a835990dbb8b5578dfee
DevlynHall <delvyn.hal@gmail.com>
2025-09-27 13:11:03 +0930

    finished boss

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
+var life_time = 8
 func _process(delta: float) -> void:
-	self.position += Vector2(1, 0).rotated(self.rotation)
-	
+	life_time -= delta
 	if ($RayCast2D.is_colliding()):
 		print("hit")
 		var hud = get_tree().get_first_node_in_group("hud")
 		if hud:
 			hud.add_hit()
+	if life_time <= 0:
 		queue_free()
-	if not get_viewport().get_visible_rect().has_point(global_position):
-		queue_free()
+		
+	
